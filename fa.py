dc1269022ada78b704ff791feaf5cc27b2061b2b
YMaster7 <helium747@gmail.com>
2022-03-20 13:15:44 +0800

    add class `random_fa` to test the program

--- fa.py
+++ fa.py
@@ -1,3 +1,7 @@
+import random
+import string
+
+
 class fa:
     def __init__(self, states, alphabet, start_state, final_states, transitions):
         self.states = states
@@ -23,3 +27,20 @@ class fa:
             'final_states': self.final_states,
             'transitions': self.transitions,
         }
+
+
+class random_fa(fa):
+    def __init__(self, Nstates=5, Nalphabet=2, Nfinal_states=1, Ntransitions=2):
+        self.states = list(range(Nstates))
+        self.alphabet = list(string.ascii_lowercase)[0:Nalphabet]
+        self.start_state = 0
+        self.final_states = random.sample(
+            self.states[1:], random.randint(1, Nfinal_states)
+        )
+        self.transitions = {}
+        for s in self.states:
+            for a in self.alphabet:
+                self.transitions.setdefault(s, {})
+                self.transitions[s][a] = random.sample(
+                    self.states, random.randint(1, Ntransitions)
+                )
